1f62fa5c89aec17371e2d2b492c0f2461e17ae23
Robert  Dinu <vjroby@gmail.com>
2019-02-26 21:14:59 +0100

    Fixed imports.

--- graphql-spqr-spring-boot-autoconfigure/src/main/java/io/leangen/graphql/spqr/spring/autoconfigure/SpqrAutoConfiguration.java
+++ graphql-spqr-spring-boot-autoconfigure/src/main/java/io/leangen/graphql/spqr/spring/autoconfigure/SpqrAutoConfiguration.java
@@ -8,7 +8,11 @@ import io.leangen.graphql.ExtensionProvider;
 import io.leangen.graphql.GeneratorConfiguration;
 import io.leangen.graphql.GraphQLSchemaGenerator;
 import io.leangen.graphql.execution.ResolverInterceptorFactory;
-import io.leangen.graphql.generator.mapping.*;
+import io.leangen.graphql.generator.mapping.ArgumentInjector;
+import io.leangen.graphql.generator.mapping.InputConverter;
+import io.leangen.graphql.generator.mapping.OutputConverter;
+import io.leangen.graphql.generator.mapping.SchemaTransformer;
+import io.leangen.graphql.generator.mapping.TypeMapper;
 import io.leangen.graphql.generator.mapping.strategy.AbstractInputHandler;
 import io.leangen.graphql.generator.mapping.strategy.InterfaceMappingStrategy;
 import io.leangen.graphql.metadata.messages.MessageBundle;
@@ -46,7 +50,14 @@ import org.springframework.util.StringUtils;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedType;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 @Configuration
 @ConditionalOnClass(GraphQLSchemaGenerator.class)
@@ -129,12 +140,12 @@ public class SpqrAutoConfiguration {
 
     @Bean
     public ExtensionProvider<GeneratorConfiguration, TypeMapper> pageTypeMapper() {
-        return ((config, defaults) -> defaults.prepend( new PageRequestMapper()));
+        return ((config, defaults) -> defaults.prepend(new PageRequestMapper()));
     }
 
     @Bean
     public ExtensionProvider<GeneratorConfiguration, OutputConverter> pageTypeAdapter() {
-        return ((config, defaults) -> defaults.prepend( new PageAdapter()));
+        return ((config, defaults) -> defaults.prepend(new PageAdapter()));
     }
 
     @Bean
@@ -242,15 +253,15 @@ public class SpqrAutoConfiguration {
                 .filter(spqrBean -> spqrBean.getScope().equals(BeanScope.SINGLETON))
                 .forEach(
                         spqrBean ->
-                        schemaGenerator.withOperationsFromSingleton(
-                            spqrBean.getSpringBean(),
-                            spqrBean.getType(),
-                            spqrBean.resolverBuilders.stream()
-                                    .map(resolverBuilderBeanIdentity -> findQualifiedBeanByType(resolverBuilderBeanIdentity.getResolverType(),
-                                            resolverBuilderBeanIdentity.getValue(),
-                                            resolverBuilderBeanIdentity.getQualifierType()))
-                                    .toArray(ResolverBuilder[]::new)
-                        )
+                                schemaGenerator.withOperationsFromSingleton(
+                                        spqrBean.getSpringBean(),
+                                        spqrBean.getType(),
+                                        spqrBean.resolverBuilders.stream()
+                                                .map(resolverBuilderBeanIdentity -> findQualifiedBeanByType(resolverBuilderBeanIdentity.getResolverType(),
+                                                        resolverBuilderBeanIdentity.getValue(),
+                                                        resolverBuilderBeanIdentity.getQualifierType()))
+                                                .toArray(ResolverBuilder[]::new)
+                                )
                 );
     }
 
@@ -262,27 +273,27 @@ public class SpqrAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public GraphQL graphQL(GraphQLSchema schema){
+    public GraphQL graphQL(GraphQLSchema schema) {
         GraphQL.Builder builder = GraphQL.newGraphQL(schema);
         return builder.build();
     }
 
     private <T> T findQualifiedBeanByType(Class<? extends T> type, String qualifierValue, Class<? extends Annotation> qualifierType) {
         final NoSuchBeanDefinitionException noSuchBeanDefinitionException = new NoSuchBeanDefinitionException(qualifierValue, "No matching " + type.getSimpleName() +
-                " bean found for qualifier " + qualifierValue + " of type " + qualifierType.getSimpleName() +" !");
+                " bean found for qualifier " + qualifierValue + " of type " + qualifierType.getSimpleName() + " !");
         try {
 
             if (StringUtils.isEmpty(qualifierValue)) {
-                if (qualifierType.equals(Qualifier.class)){
+                if (qualifierType.equals(Qualifier.class)) {
                     return Optional.of(
                             context.getBean(type))
                             .orElseThrow(() -> noSuchBeanDefinitionException);
                 }
                 return context.getBean(
                         Arrays.stream(context.getBeanNamesForAnnotation(qualifierType))
-                            .filter(beanName -> type.isInstance(context.getBean(beanName)))
-                            .findFirst()
-                            .orElseThrow(() -> noSuchBeanDefinitionException),
+                                .filter(beanName -> type.isInstance(context.getBean(beanName)))
+                                .findFirst()
+                                .orElseThrow(() -> noSuchBeanDefinitionException),
                         type);
             }
 
@@ -323,7 +334,7 @@ public class SpqrAutoConfiguration {
         for (String beanName : factory.getBeanDefinitionNames()) {
             BeanDefinition bd = factory.getBeanDefinition(beanName);
 
-            if(bd.getSource() instanceof StandardMethodMetadata) {
+            if (bd.getSource() instanceof StandardMethodMetadata) {
                 StandardMethodMetadata metadata = (StandardMethodMetadata) bd.getSource();
 
                 Map<String, Object> attributes = metadata.getAnnotationAttributes(GraphQLApi.class.getName());
@@ -395,7 +406,7 @@ public class SpqrAutoConfiguration {
             this.springBean = context.getBean(beanName);
             this.scope = BeanScope.findBeanScope(context, beanName);
             this.type = type;
-            this.resolverBuilders  = new ArrayList<>();
+            this.resolverBuilders = new ArrayList<>();
         }
 
         BeanScope getScope() {
@@ -439,7 +450,7 @@ public class SpqrAutoConfiguration {
         }
     }
 
-    private enum  BeanScope {
+    private enum BeanScope {
         SINGLETON,
         PROTOTYPE;
 
